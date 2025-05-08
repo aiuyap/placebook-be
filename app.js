d66baa9537ff44a04a5542f0c347eb6fa6d9f918
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-09 04:37:09 +0800

    feat: handled unsupported routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
@@ -8,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError('Route not found', 404);
+});
 
 //err handler
 app.use((err, req, res, next) => {
