79a626496bb544a100c3dceaf06d20a284a0bb93
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-09 03:50:01 +0800

    feat: added error handling

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ app.use(express.json());
 
 app.use('/api/places', placesRoutes);
 
+//err handler
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+
+  res
+    .status(err.code || 500)
+    .json({ message: err.message || 'Internal Server Error' });
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}!`);
+  console.log(`Server running on PORT http://localhost:${PORT}!`);
 });
