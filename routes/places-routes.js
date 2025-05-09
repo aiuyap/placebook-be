4e82a655ed0d546397bff5b58864387d28ffff14
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-09 16:21:48 +0800

    feat: installed express-validator and validated createNewPlace

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const placesController = require('../controllers/places-controller');
 
 const router = Router();
 
+const placeValidation = [
+  check('title').not().isEmpty(),
+  check('description').isLength({ min: 5 }),
+  check('address').not().isEmpty(),
+];
+
 router.get('/:pid', placesController.getPlaceById);
 router.get('/user/:uid', placesController.getPlacesByUserId);
 
-router.post('/', placesController.createPlace);
+router.post('/', placeValidation, placesController.createPlace);
 
 router.patch('/:pid', placesController.updatePlace);
 
